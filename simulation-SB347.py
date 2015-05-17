5925d96ee2dd9ee9482d4cd6bdf3436faf937cf1
Sophie Tandonnet <sophie.tandonnet@gmail.com>
2015-05-17 10:57:32 +0100

    Clean script and modify/add comments to make it more understandable

--- simulation-SB347.py
+++ simulation-SB347.py
@@ -12,11 +12,9 @@
 # order of female, hermaphrodite and male production) and calculates
 # for each time t the number of adult males, females and hermaphrodites
 #
-#
+# last modified: 16/05/2015
 #
 
-
-# 
 
 import sys, getopt
    
@@ -32,6 +30,8 @@ if len(sys.argv) < 2:
 
 
 # Parse command line options
+# ==========================
+
 for opt, arg in opts:
   if opt == '-a':
      print('simulation-SB347.py -f <rate of female offspring production> -h <rate of hermaphrodite offspring production> -g <file of proportion of each gender by females> -i <file of proportion of each gender by hermaphrodite')
@@ -48,29 +48,16 @@ for opt, arg in opts:
      print('simulation-SB347.py -f <rate of female offspring production> -h <rate of hermaphrodite offspring production> -g <file of proportion of each gender by females> -i <file of proportion of each gender by hermaphrodite>')
      sys.exit()
 
-# Get rates of offspring production and proportion of each gender produced at each time t
-
-#Prod_Fem_rate = open(Frate)
-#Prod_Her_rate = open(Hrate)
-
-#SexProportionFem = open(SexProportionF)
-#SexProportionHer = open(SexProportionH)
-
-#print('Female production rate is',Prod_Fem_rate)
-#print('Hermaphrodite production rate is',Prod_Her_rate)
-#print('Sex proportion by female mother is',SexProportionFem)
-#print('Sex proportion by hermaphrodite mother is',SexProportionHer)
-
-#################################################################################33
-
-# do 2 functions 
+###############################################
+# Functions to transform input files in lists #
+###############################################
 
 
 def prod_rate(rate_file_name):
     '''this function reads a rate file line by line. 
     A rate file is a one column file where each line 
     corresponds to a rate of offspring production (float between 0 and 1). 
-    The function returns a list of rates where each index of the list corresponds 
+    The function returns a list of rates (number of eggs/time unit) where each index of the list corresponds 
     to a time point and each value at an index corresponds to the number of 
     offspring produced at that particular time.'''
     rate_file = open(rate_file_name)
@@ -85,6 +72,15 @@ def prod_rate(rate_file_name):
 
 
 def sex_proportions(proportions_file_name, rate_file_name):
+    '''This function runs the previous function prod_rate on the rate_file_name
+    and reads a proportions file. Each line of the proportion file consists of 
+    three comma separated floats taking values between 0 and 1. They corespond, 
+    respectively, to the proportions of males, females and hermaphrodites). 
+    There are as many lines as time points.
+    The function sex_proportions multiplies the rate of production by each gender 
+    proportion for each time point. This results in a list of lists where each 
+    sublist corresponds to a time point of the reproductive life and contains the 
+    number of males, females and hermaphrodites (in this order)'''
     
     proportions_file = open(proportions_file_name)
     prodRate = prod_rate(rate_file_name)
@@ -109,120 +105,87 @@ def sex_proportions(proportions_file_name, rate_file_name):
     proportions_file.close()
         
     return gender_prodRate
-    
-#total_fem_prodRate = []
-#for line in Prod_Fem_rate:
-#    line = line.rstrip()
-#    total_fem_prodRate += [float(line)] # x by a factor
-
-#line_counter = 0    
-#for proportions in SexProportionFem:
-#    line_counter += 1
-#    
-#    comma_indexes = []
-#    i=0
-#    while i < len(proportions):
-#        if i == ',':
-#            comma_indexes += [i]
-#        i += 1
-#    
-#    Male_proportion = float(proportions[0:comma_indexes[0]]) * total_fem_prodRate[line_counter]
-#    Female_proportion = float(proportions[comma_indexes[0]:comma_indexes[1]]) * total_fem_prodRate[line_counter]
-#    Hermaphrodite_proportion = float(proportions[comma_indexes[1]:]) * total_fem_prodRate[line_counter]
-#    
-#    fem_prodRate += [[Male_proportion],[Female_proportion],[Hermaphrodite_proportion]]
             
-
 ####################
-# initial settings #
+# Initial settings #
 ####################
-
-# TODO: EXPLAIN LIFETIME OF HERM FEM AND MALE
 
 day_time_points = 3 #population monitored every 8h, therefore 3 times in a day
 
 
-#time_of_egg = 2 # 2 x 8h
-#time_of_L1 = 1*day_time_points 
-#time_of_L2 = 2
-#time_of_L3 = 1 # was 1
-#time_of_dauer = 1*day_time_points + 1
-time_of_L4 = 1
-
 # Hermaphrodite settings
 # ----------------------
 
-H_larval_time = 3 * day_time_points # 3days as larvae
-#H_larval_time =  time_of_egg + time_of_L1 + time_of_L2 + time_of_dauer + time_of_L4 # number of hours the hermaphrodite is a larvae
-H_adult_time = 5*day_time_points+1
-lifespanH = H_larval_time + H_adult_time
+H_larval_time = 3 * day_time_points # Time period where the hermaphrodite is a larvae (3days)
+H_adult_time = 5*day_time_points+1 # Time period where the hermaphrodite is reproducing (5days and 8h)
+lifespanH = H_larval_time + H_adult_time # Total lifespan of a hermaphrodite (from egg to end of reproduction)
 
-time_before_dauer = 2 * day_time_points # 2 days
-#time_before_dauer =  time_of_egg + time_of_L1 + time_of_L2
+time_before_dauer = 2 * day_time_points # Time of egg + L1 + L2 (2 days)
+time_of_L4 = 1
 time_after_dauer = time_of_L4 + H_adult_time
 
 # Female settings
 # ---------------
 
-F_larval_time = 2 * day_time_points + 1 # or +2
-#F_larval_time = time_of_egg + time_of_L1 + time_of_L2 + time_of_L3 + time_of_L4 # number of hours the female is a larvae (3 days)
-F_adult_time = 5*day_time_points+1 # time the female is adult (5 days and 8 hours (=1 time point))
-lifespanF = F_larval_time + F_adult_time
+F_larval_time = 2 * day_time_points + 1 # Time period where the female is a larvae (2days and 8h)
+F_adult_time = 5*day_time_points+1 # Time period where the hermaphrodite is reproducing (5 days and 8 hours (=1 time point))
+lifespanF = F_larval_time + F_adult_time # Total lifespan of a hermaphrodite (from egg to end of reproduction)
 
 # Male settings
 # -------------
 
-M_larval_time = 2 * day_time_points + 1
-#M_larval_time = time_of_egg + time_of_L1 + time_of_L2 + time_of_L3 + time_of_L4 # number of hours the male is a larvae
-M_adult_time = 5*day_time_points+1
-lifespanM = M_larval_time + M_adult_time
-
-##################
-# Checking if initial setting are in accord with input files
-################
-
-#Checking_number_of_lines(proportion_file_1, proportion_file_2, rate_file_1, rate_file_2):
-#    files 
-#    for line in open(SexProportionF):
-#        number_of_lines_SexProportionF += 1
+M_larval_time = 2 * day_time_points + 1 # Time period where the male is a larvae (2days and 8h)
+M_adult_time = 5*day_time_points+1 # Time period where the male is reproducing (5 days and 8 hours (=1 time point))
+lifespanM = M_larval_time + M_adult_time # Total lifespan of a male (from egg to end of reproduction)
 
 
+# initial population
+# ------------------
 
-######################
-# initial population #
-######################
+# The population starts with 1 dauer larvae that arrive in an optimal environment (abundance of food)
 
-N_fem = [0]*lifespanF # Number of females at every unit of time of their life
-N_mal = [0]*lifespanM # Number of males at every unit of time
-N_her = [0]*time_before_dauer + [0] + [1] + [0]*time_after_dauer # Number of hermaphrodites at every unit of time, Herm live 24h more, the [0] and [1] represent the dauer larvae time (16h).
+N_fem = [0]*lifespanF # Number of females at every unit of time (index of the list) of their life. At the begining of the simulation there are no females.
+N_mal = [0]*lifespanM # Number of males at every unit of time. At the begining of the simulation there are no males.
+N_her = [0]*time_before_dauer + [0] + [1] + [0]*time_after_dauer # Number of hermaphrodites at every unit of time. The simulation begins with one dauer larvae (hermaphrodite-fated).The [0] and [1] represent the dauer larvae time (16h).
 
-
-# Assert
+# Assert: Testing if the hermaphrodite lifespan in the same in the lists "lifespanH" (initial settings) and "N_her" (initial population)
 
 if len(N_her) != lifespanH: print("Length of hermaphrodite list N_her different from hermaphrodite lifespan")
 
+
+# Number of offspring of each gender produced at each time point by females and hermaphrodites
+# --------------------------------------------------------------------------------------------
+
+# The order in the sublists is: [males,females,hermaphrodite]
+# Prepending zeros to production rate vector as larvas do not reproduce
+
+fem_prodRate = [[0,0,0]]*F_larval_time+sex_proportions(SexProportionF, Frate)
+her_prodRate = [[0,0,0]]*H_larval_time+sex_proportions(SexProportionH, Hrate)
+
+#EXAMPLE: her_prodRate = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[40,80,40],[20,80,80],[20,80,200],[15,40,100],[10,15,70],[5,5,50],[0,25,15]]
+#EXAMPLE: fem_prodRate = [[0,0,0],[0,0,0],[0,0,0],[30,30,70],[20,20,150],[10,10,200],[5,5,200],[2,2,80],[2,2,50],[0,0,40]]
+
 # Storing the length of the previous vectors (which represent the total units of time ie, the lifespan)
+# -----------------------------------------------------------------------------------------------------
 
 L_fem = len(N_fem)
 L_her = len(N_her)
 L_mal = len(N_mal)
 
-# Production rate of each gender at each moment of their life (unit of time),
-# The order is: [males,females,hermaphrodite]
-# Prepend zeros to production rate vector as larvas do not reproduce
-
-fem_prodRate = [[0,0,0]]*F_larval_time+sex_proportions(SexProportionF, Frate)
-her_prodRate = [[0,0,0]]*H_larval_time+sex_proportions(SexProportionH, Hrate)
-
-
-#EXAMPLE: her_prodRate = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[40,80,40],[20,80,80],[20,80,200],[15,40,100],[10,15,70],[5,5,50],[0,25,15]]
-#EXAMPLE: fem_prodRate = [[0,0,0],[0,0,0],[0,0,0],[30,30,70],[20,20,150],[10,10,200],[5,5,200],[2,2,80],[2,2,50],[0,0,40]]
 
 # header of output
+# ----------------
+
 print('#M','#F','#H','%M','%F','%H', sep="\t")
 
+#####################################################################
+# Simulating the growth of the population from the initial settings #
+#####################################################################
+
 # Number of units of time to run the program 
-N_iterations = 100 # each iteration corresponds to 24 / day_time_point (8h)
+# ------------------------------------------
+
+N_iterations = 102 # each iteration corresponds to 8h. One day is therefore 3 iterations. 102 iterations = 34 days.
 current_iteration = 0
 
 while current_iteration < N_iterations:
@@ -232,7 +195,6 @@ while current_iteration < N_iterations:
     N_fem_res = [0]*L_fem
     N_her_res = [0]*L_her
     N_mal_res = [0]*L_mal
-    # print(N_mal,N_fem,N_her)
     
 # Time passes -> individuals age and are passed in the following index of the list
     
@@ -245,7 +207,7 @@ while current_iteration < N_iterations:
     for i in range(1,L_her):
         N_her_res[i] = N_her[i-1] 
 
-# Adult females and hermaphrodites produce eggs: We add eggs according to the rate of production of each gender and age
+# Adult females and hermaphrodites produce eggs: We add eggs (index 0) according to the rate of production of each gender and age
     i = 0
     while i < L_fem:
         N_fem_res[0] += N_fem[i]*fem_prodRate[i][1]
@@ -260,10 +222,12 @@ while current_iteration < N_iterations:
         N_mal_res[0] += N_her[i]*her_prodRate[i][0]
         i += 1
 
-# update population                       
+# update population
+                       
     N_fem = N_fem_res
     N_her = N_her_res 
     N_mal = N_mal_res 
+
 
 # Total number of adults
 
@@ -274,15 +238,6 @@ while current_iteration < N_iterations:
     result_A = [Total_Amal,Total_Afem,Total_Aher]
     total_A = sum(result_A)
 
-# Total number of L1s
-# TODO parametrize or remove
-
-    L1_fem = sum(N_fem[7:9])
-    L1_mal = sum(N_mal[7:9])
-    L1_her = sum(N_her[7:9])
-    
-    result_L1 = [L1_mal,L1_fem, L1_her]
-    total_L1 = sum(result_L1)
 
 # Total number of individuals of each gender
 
@@ -290,23 +245,22 @@ while current_iteration < N_iterations:
     Total_her = sum(N_her)
     Total_mal =sum(N_mal)
     
-    result2 = [Total_mal,Total_fem,Total_her]
-    total2 = sum(result2)    
+    result = [Total_mal,Total_fem,Total_her]
+    total = sum(result)    
     
-    
-#    print('total number of adults:', total,result, 'total number of individuals:', total2, result2)
-# TODO change wanted by output
+# Choosing population for output (Adults or mixed individuals)
 
-    population_wanted = result_A
-    total_population_wanted = total_A
+    population_output = result_A
+    total_population_output = total_A
 
-    if total_population_wanted != 0:
-        proportion = [x * 100/total_population_wanted for x in population_wanted]
+    if total_population_output != 0:
+        proportion = [x * 100/total_population_output for x in population_output]
     else:
         proportion = [0,0,0]
         
 # print result to Standard output
-    print(population_wanted[0] , population_wanted[1] , population_wanted[2] , proportion[0] , proportion[1] , proportion[2] , sep="\t")
+
+    print(population_output[0] , population_output[1] , population_output[2] , proportion[0] , proportion[1] , proportion[2] , sep="\t")
     
     current_iteration += 1
     
